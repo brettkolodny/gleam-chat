616eadb0501ca5a90fc47a2c268ae1af6e3c50da
brettkolodny <brettkolodny@gmail.com>
2022-12-28 10:22:55 -0500

    remove html from build

--- front-end/build.js
+++ front-end/build.js
@@ -5,26 +5,6 @@ const fs = require("fs");
 const watch = process.argv.includes("--watch");
 const isProd = process.env.NODE_ENV === "production";
 
-const writeHtml = () => {
-  const bundle = fs.readFileSync("./dist/bundle.js");
-
-  const htmlFile = `<!DOCTYPE html>
-  <html lang="en">
-    <head>
-      <meta charset="UTF-8" />
-      <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      <title>Document</title>
-    </head>
-    <body></body>
-    <script>
-        ${bundle.toString()}
-    </script>
-  </html>`;
-
-  fs.writeFileSync("./dist/index.html", htmlFile);
-};
-
 esbuild
   .build({
     entryPoints: ["src/index.js"],
@@ -39,5 +19,4 @@ esbuild
       }), // options are documented below
     ],
   })
-  .catch((e) => (console.error(e), process.exit(1)))
-  .then(() => writeHtml());
+  .catch((e) => (console.error(e), process.exit(1)));
